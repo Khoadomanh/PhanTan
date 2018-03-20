26f35d6c57089b1ae899050be7086921a88c180e
KHOADO-PC\nagat <nagatana9x@gmail.com>
2018-03-20 16:26:22 +0700

    update giao dien thong tin cay + giao dien chon chuc nang

--- app/src/main/java/com/example/nagat/phantan/fragment/FragmentOne.java
+++ app/src/main/java/com/example/nagat/phantan/fragment/FragmentOne.java
@@ -1,11 +1,15 @@
 package com.example.nagat.phantan.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.nagat.phantan.InformationTreeActivity;
 import com.example.nagat.phantan.R;
 
 /**
@@ -13,11 +17,21 @@ import com.example.nagat.phantan.R;
  */
 
 public class FragmentOne extends Fragment {
+    private TextView tvInforTree;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.content_gallery, container, false);
+        View view = inflater.inflate(R.layout.content_gallery, container, false);
+        tvInforTree = view.findViewById(R.id.thongTinCay);
+        tvInforTree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), InformationTreeActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 
@@ -26,5 +40,11 @@ public class FragmentOne extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Menu 1");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
