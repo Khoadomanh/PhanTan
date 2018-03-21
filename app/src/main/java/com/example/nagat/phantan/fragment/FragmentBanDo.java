9269e30c331cf277baf2ad29b13732cb469bffab
KHOADO-PC\nagat <nagatana9x@gmail.com>
2018-03-21 14:55:44 +0700

    fix maps + find vị trí hiện tại người dùng  + item schedule

--- app/src/main/java/com/example/nagat/phantan/fragment/FragmentBanDo.java
+++ app/src/main/java/com/example/nagat/phantan/fragment/FragmentBanDo.java
@@ -1,28 +1,30 @@
 package com.example.nagat.phantan.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.example.nagat.phantan.InformationTreeActivity;
 import com.example.nagat.phantan.R;
 
 /**
  * Created by nagat on 20/3/2018.
  */
 
-public class FragmentOne extends Fragment {
+public class FragmentBanDo extends Fragment {
 //    private TextView tvInforTree;
+    public static FragmentBanDo init() {
+            return new FragmentBanDo();
+        }
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
         View view = inflater.inflate(R.layout.activity_test_map, container, false);
+        MapFragment mapFragment = new MapFragment();
+        getChildFragmentManager().beginTransaction().replace(R.id.myMap,  mapFragment).commit();
 //        tvInforTree = view.findViewById(R.id.thongTinCay);
 //        tvInforTree.setOnClickListener(new View.OnClickListener() {
 //            @Override
