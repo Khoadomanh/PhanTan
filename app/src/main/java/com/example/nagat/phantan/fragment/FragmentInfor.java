eb94567aa158a95479279f6a912bedc1aff4939f
honganhdo <dohong1803@gmail.com>
2018-04-11 21:18:40 +0700

    update infor

--- app/src/main/java/com/example/nagat/phantan/fragment/FragmentInfor.java
+++ app/src/main/java/com/example/nagat/phantan/fragment/FragmentInfor.java
@@ -6,17 +6,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.nagat.phantan.R;
+import com.example.nagat.phantan.model.User;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Win 8.1 Version 2 on 22/03/2018.
  */
 
+
 public class FragmentInfor extends Fragment {
+
+    @BindView(R.id.tv_name_infor)
+    TextView tvNameInfor;
+    @BindView(R.id.tv_name_infor_large)
+    TextView tvNameInforLarge;
+    @BindView(R.id.btn_edit_name_display)
+    Button btnEditNameDisplay;
+    @BindView(R.id.tv_sex_user)
+    TextView tvSexUser;
+    @BindView(R.id.role)
+    TextView tvRoleUser;
+    @BindView(R.id.ln_change_password)
+    LinearLayout lnChangePassword;
+    @BindView(R.id.img_avatar_infor)
+    CircleImageView imageAvatar;
+
+
+    User user = new User();
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
 
@@ -24,5 +54,9 @@ public class FragmentInfor extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Thông tin người dùng");
+    }
+
+    private void bindView() {
+
     }
 }
